6aa0bd7d7b2ea5ff7c7b9c721675352610a1c4d2
Leewen-Adianah-L-Lumba-101 <144773475+Leewen-Adianah-L-Lumba-101@users.noreply.github.com>
2025-01-03 10:27:28 +0400

    Create API_Application.py

---
+++ Debug and Testing/TestingForFrameDelete.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  1 15:06:33 2025
+
+@author: hp
+"""
+
+test = ["no1", "no2", "no3", "no4", "no5"]
+firstpart = []
+secondpart = []
+
+# print(length)
+
+count = 0
+
+for i in test:
+    
+    if (count % 2) == 0:
+        print(i)
+
+    else:
+        pass
+    
+    count += 1
+
+
+# count = 0
+# i = 0
+# count2 = len(test)
+# widgetstokeep = test[0:3]
+# print(widgetstokeep)
+
+# totals = test
+
+# empty = None
+
+# if empty is None:
+#     print("Empty")
+    
+# else:
+#     print("Full")
+
+
+
+# for i in range(count2):
+    
+#     if count >= 3:
+#         print(test[i])
+#         test.remove(i)
+#         count2 -= 1
+
+#     else:
+#         pass
+    
+#     i += 1
+#     count += 1
+
+
+# print(test)
+
+
+
+# for i in test:
+#     if count >= 2:
+#         print(i)
+#         test.remove(i)
+#         i = 0
+#         count = 0
+
+#     else:
+#         pass
+
+#     count += 1
+
+
+# print(test)
+
+# if totals[-1] == i:
+#     totals = totals[:-1]
+# for i in test:
+    
+#     if i == widgetstokeep[count]:
+#         print(i)
+#         break
+        
+#     else:
+#         break
+    
+#     count += 1
+
+
+
+
+# print(test)
+
+    
+    
+# if count >= 2:
+#     for i in range(count):
+#         test.remove(i)
+        
+        
+#if length of list is more than 2 keep deleting until item is 
+    
+    # count = 0
+    # indextodel = -1
+    
+    # totalwidgets = frame.winfo_children()
+    
+    # for widget in frame.winfo_children():
+    #     if count >= 4:
+    #         if totalwidgets[indextodel] == widget:
+    #             print(widget)
+                
+    #         else:
+    #             pass
+    #     else:
+    #         pass
+        
+    #     count += 1
+    
+    
+    
+    # Check if the total no of widgets exceeds 3, if it exceeds 3, delete every widget
+    # child after the third index. this will keep the results and the comboboxes (hopefully)
+
+    # for widget in frame.winfo_children():
+    #     count += 1
+    
+    # if count >= 4:
+    #     for widget in totalwidgets:
+    #         print(f"{count}, {widget}")
+    #         count += 1
+    # else: 
+    
+    
+    # if count >= 4: 
+
+    # for widget in frame.winfo_children:
+    #     count += 1
+    #     if count >= 4: 
+    #         widget.destroy()
+        
+    # else:
+    #     for widget in frame.winfo_children():
+    #         widget.destroy()
+            
+
+        
+        
--- README.md
+++
@@ -1,3 +0,0 @@
-# Assessment-2-Advanced-Programming
-
-# Note: Once again the libraries mentioned in the utility app credits are from https://customtkinter.tomschimansky.com/ and [https://pypi.org/project/tmdb3/](https://pypi.org/project/tmdbv3api/)
